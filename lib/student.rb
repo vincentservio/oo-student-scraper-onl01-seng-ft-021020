271f338d2dd924bef2a16efaeba26c23c549893d
Vincent Servio <vincentservio@gmail.com>
2020-03-09 05:11:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    (students_array)
+    
     students_array.each do |student_hash|
       Student.new(student_hash)
   end
