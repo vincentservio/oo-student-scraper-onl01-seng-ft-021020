3751c4088fc32cc590179e0e4dcd4b7929f2af48
Vincent Servio <vincentservio@gmail.com>
2020-03-09 05:17:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,52 @@
 require 'open-uri'
 require 'pry'
 
+
+
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+ def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    student_cards = doc.css(".student-card a")
+    student_cards.collect do |element|
+      {:name => element.css(".student-name").text ,
+        :location => element.css(".student-location").text,
+        :profile_url => element.attr('href')
+      }
+    end
   end
-
+  
+  
+  
   def self.scrape_profile_page(profile_url)
     
-  end
+    student = {} 
+    profile_page = Nokogiri::HTML(open(profile_url))
+    links = profile_page.css(".social-icon-container").children.css("a").map {|x| x.attribute("href").value}
+  
+  links.each do |link|
+      
+    if link.include?("linkedin")
+      student[:linkedin] = link 
+  elsif link.include?("github")
+      student[:github] = link 
+  elsif link.include?("twitter")
+      student[:twitter] = link 
+    else 
+      student[:blog] = link 
+    end 
+  end 
+  
 
+  
+    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+
+    student
+  end 
 end
-
+#name student.css("h4").text
+#student.css("p.student-location").text
+#student.css(h3.view-profile-text).text
+#location 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
     
+    students_array.each do |student_hash|
+      Student.new(student_hash)
   end
+end
 
   def add_student_attributes(attributes_hash)
-    
+    self.send("twitter=", attributes_hash[:twitter])
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("github=", attributes_hash[:github])
+    self.send("blog=", attributes_hash[:blog])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
+    self.send("bio=", attributes_hash[:bio])
   end
 
   def self.all
-    
+    @@all
   end
 end
 
