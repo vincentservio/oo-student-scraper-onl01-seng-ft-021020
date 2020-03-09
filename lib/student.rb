ab249b3b5589b84edff09acc86dc0ffb4f8131cb
Vincent Servio <vincentservio@gmail.com>
2020-03-09 05:16:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,13 +17,14 @@ class Student
       Student.new(student_hash)
   end
 end
+
   def add_student_attributes(attributes_hash)
     self.send("twitter=", attributes_hash[:twitter])
     self.send("linkedin=", attributes_hash[:linkedin])
     self.send("github=", attributes_hash[:github])
     self.send("blog=", attributes_hash[:blog])
     self.send("profile_quote=", attributes_hash[:profile_quote])
-    self.send("bio=", attributes_hash[:bio]))
+    self.send("bio=", attributes_hash[:bio])
   end
 
   def self.all
