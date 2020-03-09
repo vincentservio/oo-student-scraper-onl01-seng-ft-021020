77f9cae8eade41cf657a991569686cfdbbd7a906
Vincent Servio <vincentservio@gmail.com>
2020-03-09 04:55:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
